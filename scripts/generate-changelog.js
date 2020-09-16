d34a3d9775fcc002adf837b1964be6749728f37d
Tristan Lee <tristanlee85@gmail.com>
2020-09-16 16:43:32 -0400

    test

--- scripts/generate-changelog.js
+++ scripts/generate-changelog.js
@@ -3,5 +3,6 @@ const fs = require('fs');
 
 fs.writeFileSync('./CHANGELOG.md', data.reduce((acc, v, i) =>
 `${acc}
+## [${v.tag_name}](${v.html_url}) (${v.published_at.split('T')[0]})
 ${v.body}
 ${i < data.length - 1 && '---' || ''}`, '').trim());
