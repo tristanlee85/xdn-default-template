7b63bba9341928b662230273dee61e33e2b5eea9
Tristan Lee <tristanlee85@gmail.com>
2020-09-16 17:35:54 -0400

    test

--- scripts/generate-changelog.js
+++ scripts/generate-changelog.js
@@ -1,8 +1,14 @@
-const data = require('../release-data.json');
 const fs = require('fs');
+
+if (!process.env.RELEASE_DATA) {
+  throw 'RELEASE_DATA env var unavailable. Exiting.'
+}
+
+const data = JSON.parse(process.env.RELEASE_DATA);
 
 fs.writeFileSync('./CHANGELOG.md', data.filter(v => !v.draft).reduce((acc, v, i) =>
 `${acc}
 ## [${v.tag_name}](${v.html_url}) (${v.published_at.split('T')[0]})
 ${v.body}
-${i < data.length - 1 && '---' || ''}`, '').trim());
+${i < data.length - 1 && '---' || ''}`, '')
+.trim());
