33f3db8a1c80a094572b30427b454f4dac9da331
Tristan Lee <tristanlee85@gmail.com>
2020-09-17 11:40:54 -0400

    test

--- scripts/generate-changelog.js
+++ scripts/generate-changelog.js
@@ -1,14 +1,24 @@
+const fetch = require('node-fetch')
 const fs = require('fs');
+const { parse } = require('path');
 
-if (!process.env.RELEASE_DATA) {
-  throw 'RELEASE_DATA env var unavailable. Exiting.'
+const [repo, token] = process.argv.slice(2);
+
+if (!repo || !token) {
+  throw 'API token or repository not present. Exiting.';
 }
 
-const data = JSON.parse(process.env.RELEASE_DATA);
-
-fs.writeFileSync('./CHANGELOG.md', data.filter(v => !v.draft).reduce((acc, v, i) =>
-`${acc}
-## [${v.tag_name}](${v.html_url}) (${v.published_at.split('T')[0]})
-${v.body}
-${i < data.length - 1 && '---' || ''}`, '')
-.trim());
+fetch(`https://api.github.com/repos/${repo}/releases`, {
+  headers: {
+    authorization: `token ${token}`
+  }
+})
+  .then(res => res.json())
+  .then(data => {
+    fs.writeFileSync('./CHANGELOG.md', data.filter(v => !v.draft).reduce((acc, v, i) => `
+      ${acc}
+      ## [${v.tag_name}](${v.html_url}) (${v.published_at.split('T')[0]})
+      ${v.body}
+      ${i < data.length - 1 && '---' || ''}
+    `, '').trim().replace(/^\s+/gm, ''));
+  });
